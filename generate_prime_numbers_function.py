71940bc07d77da693b03aa38505317b8074f8854
slubambo <slubambo57@gmail.com>
2017-04-18 12:57:13 +0300

    Changes to Function to include test if n is an integer

--- generate_prime_numbers_function.py
+++ generate_prime_numbers_function.py
@@ -1,11 +1,15 @@
 def generate_prime_numbers_function(n):
 
-    out = list()
-    for num in range(2, n + 1):
-        prime = True
-        for i in range(2, num):
-            if num % i == 0:
-                prime = False
-        if prime:
-            out.append(num)
-    return out
+    while isinstance(n, int):
+
+        out = list()
+        for num in range(2, n + 1):
+            prime = True
+            for i in range(2, num):
+                if num % i == 0:
+                    prime = False
+            if prime:
+                out.append(num)
+        return out
+
+    raise TypeError('n must be an integer')
