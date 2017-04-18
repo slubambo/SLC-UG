1f1a6ba26c6c2e479f2062b07e454b05884ca468
slubambo <slubambo57@gmail.com>
2017-04-18 12:48:12 +0300

    Further changes in Test Case File

--- PrimeNumberTestCases.py
+++ PrimeNumberTestCases.py
@@ -13,11 +13,8 @@ class TestPrimeNumberFunction(unittest.TestCase):
     def testIfNIsGreaterThan0(self):
         self.assertGreater(self, self.n, 0, 'n must be greater than 0')
 
-    def testIfNIsGreaterThan2(self):
-        self.assertGreater(self, self.n, 2, 'n must be greater than 2 beacuse there is no prime number between 0 and 2')
-
     def testWhenNIs2(self):
-        self.result = f.generate_prime_numbers_function(2)
+        self.result = f.generate_prime_numbers_function(1)
         assert self.result.__len__() == 0
 
     def testWhenNIsaString(self):
@@ -27,3 +24,7 @@ class TestPrimeNumberFunction(unittest.TestCase):
     def testWHenNIsNegative(self):
         self.result = f.generate_prime_numbers_function(-10)
         assert self.result == "n must be greater than 0"
+
+    def testWHenNIsNegative(self):
+        self.result = f.generate_prime_numbers_function(10)
+        assert self.result == [2, 3, 5, 7]
